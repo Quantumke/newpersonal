ae53307745e00e68b87bc942f02bb5fb5c4ed079
GodMode <nguruben@gmail.com>
2016-05-05 00:54:57 +0300

    onclick fns

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+function goportofolio()
+	{
+
+		window.location.href="work.php";
+	}
+
+function gohome()
+	{
+
+		window.location.href="index.php";
+	}
+
+
+
+
+function golabs()
+	{
+
+		window.location.href="lab.php";
+	}
+function gocontact()
+	{
+
+		window.location.href="contact.php";
+	}
+function contact()
+	{
+
+		window.location.href="post.php";
+	}
+
+
